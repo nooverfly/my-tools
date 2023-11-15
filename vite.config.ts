2067a402f7103394a4ec9b77c4c8f1abd8b635eb
齐昊 <qihao.wu@tuya.com>
2023-11-15 12:01:59 +0800

    修改部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-tool/",
+  base: "/my-tools/",
   build: {
     outDir: "./docs",
   },
